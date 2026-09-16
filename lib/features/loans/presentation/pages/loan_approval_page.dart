259ac28a3a5eea0d616848fae235af3da3078a56
adityaa369 <adityaamruthaluri369@gmail.com>
2026-09-16 12:07:24 +0530

    style: remove duplicate and unused imports in loan_approval_page

--- lib/features/loans/presentation/pages/loan_approval_page.dart
+++ lib/features/loans/presentation/pages/loan_approval_page.dart
@@ -3,13 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 
 import '../../../../config/theme.dart';
 import '../../../../config/constants.dart';
